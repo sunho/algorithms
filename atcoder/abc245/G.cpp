2c5181aabbfe34d9f2eb8852be2194027f11f7a2
Sunho Kim <ksunhokim123@gmail.com>
2022-03-27 21:50:00 +0900

    Add solution for problem `G - Foreign Friends` Link: https://atcoder.jp/contests/abc245/tasks/abc245_g Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/abc245/submissions/30512456

--- atcoder/abc245/G.cpp
+++ atcoder/abc245/G.cpp
@@ -9,7 +9,7 @@ using vi = vector<int>;
 #define all(x) begin(x), end(x)
 #define trav(x,a) for(auto& x : (a))
 #define endl '\n'
-
+#define inf 1e18+10
 template<class T> bool ckmin(T&a, const T&b) { bool B = a > b; a = min(a,b); return B; }
 template<class T> bool ckmax(T&a, const T&b) { bool B = a < b; a = max(a,b); return B; }
 template <typename T,typename U> pair<T,U> operator+(const pair<T,U> & l,const pair<T,U> & r) { return {l.first+r.first,l.second+r.second}; }
@@ -24,18 +24,17 @@ int main() {
     rep(i,0,n) cin >> A[i];
 
     vector<vector<pii>> g(n);
-    const ll inf = 1e18+10;
-    priority_queue<tuple<ll,int, int>, vector<tuple<ll,int,int>>, greater<>> q;
-    vector<vector<pair<ll,int>>> d(n, vector<pair<ll,int>>(2, {inf, -1}));
+    priority_queue<tuple<ll,int,int>, vector<tuple<ll,int,int>>, greater<>> q;
+    vector<ll> d(n, -1);
+    vi dc(n,-1);
     vi seen(n);
-    vector<ll> dd(n, inf);
+    vector<ll> dd(n, -1);
 
     rep(i,0,l) {
         int x;
         cin >> x;
         --x;
-        q.push({0, x, 0});
-        d[x][0] = {0, A[x]};
+        q.push({0, x, A[x]});
     }
     rep(i,0,m) {
         int x,y,z;
@@ -45,44 +44,30 @@ int main() {
         g[y].push_back({x,z});
     }
 
-    //     a
-    // S   b
-    //     c
-    //
-    while (!q.empty()) {
-        auto [di, v, j] = q.top();
+    while(!q.empty()) {
+        auto [di, v, c] = q.top();
         q.pop();
-        if (d[v][j].x < di) continue;
-
-        for (auto edge : g[v]) {
-            auto [u, w] = edge;
-            //ll last = d[u];
-            pair<ll,int> nn = {d[v][j].x + w, d[v][j].y};
-            if (nn.y == d[u][0].y && nn.x >= d[u][0].x) {
-                continue;
-            }
-            if (nn < d[u][0]) {
-                if (nn.y != d[u][0].y) {
-                    d[u][1] = d[u][0];
-                    q.push({d[u][0].x, u, 1});
-                }
-                d[u][0]=nn;
-                q.push({d[u][0].x, u, 0});
-            } else {
-                if (ckmin(d[u][1], nn)) {
-                    q.push({d[u][1].x, u, 1});
-                }
-            }
+        if (seen[v] == 0) {
+            seen[v] = c;
+            dc[v] = c;
+            d[v] = di;
+        } else if (seen[v] != -1 && seen[v] != c) {
+            seen[v] = -1;
+            dd[v] = di;
+        } else {
+            continue;
+        }
+        trav(x, g[v]) { 
+            auto [u, w] = x;
+            q.push({di + w, u, c});
         }
     }
 
-    trav(x, d) trav(y, x) if (y.x==inf) y.x = -1;
-
     rep(i,0,n) {
-        if (d[i][0].y != A[i]) {
-            cout << d[i][0].x << " ";
+        if (A[i] != dc[i]) {
+            cout << d[i] << " ";
         } else {
-            cout << d[i][1].x << " ";
+            cout << dd[i] << " ";
         }
     }    
 
