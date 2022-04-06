bb31096b858b7a95ff51e551f5effe9534e9b2c9
Sunho Kim <ksunhokim123@gmail.com>
2022-04-06 15:00:00 +0900

    Add solution for problem `G - Sqrt` Link: https://atcoder.jp/contests/abc243/tasks/abc243_g Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/abc243/submissions/30750974

--- atcoder/abc243/G.cpp
+++ atcoder/abc243/G.cpp
@@ -8,20 +8,26 @@ template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
+ll isqrt(ll N){
+	ll sqrtN=sqrt(N)-1;
+	while(sqrtN+1<=N/(sqrtN+1))sqrtN++;
+	return sqrtN;
+}
+
 void solve() {
     ll x;
     cin >> x;
-    ll xx = (ll)sqrtl((ll)sqrtl(x));
+    ll xx = isqrt(isqrt(x));
     vector<ll> dp(xx+1);
     dp[1] = 1;
     for(ll i=2;i<=xx;i++) {
         for(ll j=1;j*j*j*j<=i;j++) {
-            dp[i] += dp[j]*((ll)sqrtl(i)-j*j+1);
+            dp[i] += dp[j]*(isqrt(i)-j*j+1);
         }
     }
     ll ans = 0;
     for(ll i=1;i<=xx;i++) {
-        ans += dp[i]*((ll)sqrtl(x) - i*i+1);
+        ans += dp[i]*(isqrt(x) - i*i+1);
     }
     cout << ans << "\n";
 }
