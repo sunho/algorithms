e8bf176558e7c07ad9812d446b2645182c5903f6
Sunho Kim <ksunhokim123@gmail.com>
2022-04-07 14:24:00 +0900

    Add solution for problem `E - Putting Candies` Link: https://atcoder.jp/contests/abc241/tasks/abc241_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc241/submissions/30768083

--- atcoder/abc241/E.cpp
+++ atcoder/abc241/E.cpp
@@ -4,52 +4,69 @@ using ll = long long;
 #define x first
 #define y second
 #define all(x) begin(x), end(x)
-template <class T>
-using pqg = priority_queue<T, vector<T>, greater<T>>;
-template <class T>
-bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
-template <class T>
-bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
 void solve() {
-  ll n, k;
-  cin >> n >> k;
-  vector<int> A(n);
-  for (int i = 0; i < n; i++) {
-    cin >> A[i];
-  }
-  ll x = 0;
-  vector<int> pos(n, -1);
-  vector<ll> sums(n);
-  int idx=0;
-  while (k && pos[x % n] == -1) {
-    pos[x % n] = idx;
-    sums[x % n] = x;
-    x += A[x % n];
-    idx++;
-    k--;
-  }
-  
-  if (k == 0) {
-    cout << x << "\n";
-    return;
-  }
-  int t = idx - pos[x % n];
-  x += k/t * (x - sums[x % n]);
-  k %= t;
-  while(k) {
-    x += A[x%n];
-    k--;
-  }
-  cout << x << "\n";
+    ll n,k;
+    cin >> n >> k;
+    vector<int> A(n);
+    for(int i=0;i<n;i++){
+        cin >> A[i];
+    }
+    set<int> visited;
+    vector<int> cycle;
+    int idx = 0;
+    while(!visited.count(idx)){
+        visited.insert(idx);
+        cycle.push_back(idx);
+        idx += A[idx];
+        idx %= n;
+    }
+    int j=0;
+    while(true) {
+        if(cycle[j] == idx) {
+            break;
+        }
+        j++;
+    }
+    vector<int> B;
+    vector<int> C;
+    for(int i=0;i<j;i++){
+        B.push_back(cycle[i]);
+    }  
+    for(int i=j;i<cycle.size();i++){
+        C.push_back(cycle[i]);
+    }
+    ll ans = 0;
+    if (k <= B.size()) {
+        for(int i=0;i<k;i++){
+            ans += A[ans % n];
+        }
+    } else {
+        for(int i=0;i<B.size();i++){
+            ans += A[ans % n];
+        }
+        k-=B.size();
+        ll sum = 0;
+        for(int i=0;i<C.size();i++){
+            sum += A[C[i]];
+        }
+        ans += sum * (k/C.size());
+        k %= C.size();
+        for(int i=0;i<k;i++){
+            ans += A[ans % n];
+        }
+    }
+    cout << ans << "\n";
 }
 
 int main() {
-  cin.sync_with_stdio(0);
-  cin.tie(0);
-  cin.exceptions(cin.failbit);
+    cin.sync_with_stdio(0); cin.tie(0);
+    cin.exceptions(cin.failbit);
 
-  solve();
+    solve();
 
-  return 0;
+    return 0;
 }
