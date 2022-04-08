a69f1c3602298817ad0ae58cd33096b21080afd4
Sunho Kim <ksunhokim123@gmail.com>
2022-04-09 02:28:00 +0900

    Add solution for problem `D - GCD Guess` Link: https://codeforces.com/contest/1665/problem/D Tags: bitmasks, chinese remainder theorem, constructive algorithms, games, interactive, math, number theory, *2000 Ref: https://codeforces.com/contest/1665/submission/153077991

--- codeforces/1665/D.cpp
+++ codeforces/1665/D.cpp
@@ -7,48 +7,21 @@ using ll = long long;
 template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-inline long long normalize(long long x, long long mod) { x %= mod; if (x < 0) x += mod; return x; }
-tuple<ll,ll,ll> exgcd(ll a, ll b) {
-    ll x = 1, y = 0;
-    ll x1 = 0, y1 = 1, a1 = a, b1 = b;
-    while (b1) {
-        ll q = a1 / b1;
-        tie(x, x1) = make_tuple(x1, x - q * x1);
-        tie(y, y1) = make_tuple(y1, y - q * y1);
-        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
-    }
-    return {a1,x,y};
-}
 
 void solve() {
-  vector<int> coprimes = {4,9,5,7,11,13,17,19,23};
-  set<int> added;
-  vector<int> a;
-  vector<int> m;
-  ll L = 1;
-  for(auto x : coprimes) L *= x;
-  for(int i=1;i<=23;i++) {
-    cout << "? " << L + i << " " << i << endl;
-    ll p;
-    cin >> p;
-    for(auto x : coprimes) {
-      if (p % x == 0 && !added.count(x)) {
-        a.push_back(((-i % x) + x) % x);
-        m.push_back(x);
-        added.insert(x);
-      }
+  int r = 0;
+  for(int i=0;i<30;i++) {
+    int k = 1 << i;
+    int k2 = (1<<(i+1));
+    int a = k - r, b = k - r + (1<<(i+1));
+    cout << "? " << a << " " << b << endl;
+    int q;
+    cin >> q;
+    if (q == k2) {
+      r += k;
     }
   }
-  ll ans = a[0];
-  ll l = m[0];
-  for(int i = 1; i < a.size(); i++)
-  {
-    auto [d, x1, y1] = exgcd(l, m[i]);
-    ans = normalize(ans + x1 * (a[i] - ans) / d % (m[i] / d) * l, l * m[i] / d);
-    l *= m[i];
-  }
-
-  cout << "! " << ans << "\n";
+  cout << "! " << r << endl;
 }
 
 int main() {
