0ccd4becaa02a5a5a62d71511f992b8e873543ba
Sunho Kim <ksunhokim123@gmail.com>
2022-04-09 02:41:00 +0900

    Add solution for problem `C - Tree Infection` Link: https://codeforces.com/contest/1665/problem/C Tags: binary search, greedy, sortings, trees, *1600 Ref: https://codeforces.com/contest/1665/submission/153079366

---
+++ codeforces/1665/C.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define x first
+#define y second
+#define all(x) begin(x), end(x)
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<vector<int>> g(n);
+  vector<int> outdeg(n);
+  for(int i=0;i<n-1;i++){
+    int u;
+    cin >> u;
+    --u;
+    outdeg[u]++;
+    g[u].push_back(i+1);
+  }
+  vector<int> idx(n);
+  iota(all(idx), 0);
+  sort(all(idx), [&](int a, int b){
+    return outdeg[a] < outdeg[b];
+  });
+  int t=0;
+  vector<int> tt(n);
+  for(int i=n-1;i>=0;i--){
+    if (outdeg[idx[i]] == 0) {
+      continue;
+    }
+    t++;
+    tt[idx[i]] = t;
+  }
+  t++;
+  //cout << t << "\n";
+  multiset<int> rem;
+  for(int i=0;i<n;i++){
+    if (outdeg[i] == 0) continue;
+    int c = outdeg[i]-(t-tt[i])-1;
+    if (c <= 0) continue;
+    rem.insert(c);
+  }
+  int tk=0;
+  while(!rem.empty()) {
+    int r = *(--rem.end());
+    if (r - tk <= 0) {
+      rem.erase(--rem.end());
+      continue;
+    }
+    r--;
+    rem.erase(--rem.end());
+    rem.insert(r);
+    tk++;
+    t++;
+  }
+  
+  cout << t << "\n";
+}
+
+int main() {
+  cin.sync_with_stdio(0); cin.tie(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+
+  return 0;
+}
