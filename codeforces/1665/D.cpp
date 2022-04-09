8eedd9d6735fcafef596fe83d648d9ad2a574e1e
Sunho Kim <ksunhokim123@gmail.com>
2022-04-10 08:13:00 +0900

    Add solution for problem `D - GCD Guess` Link: https://codeforces.com/contest/1665/problem/D Tags: bitmasks, chinese remainder theorem, constructive algorithms, games, interactive, math, number theory, *2000 Ref: https://codeforces.com/contest/1665/submission/153245759

--- codeforces/1665/D.cpp
+++ codeforces/1665/D.cpp
@@ -7,33 +7,17 @@ using ll = long long;
 template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-
 ll normalize(ll x, ll mod) { x %= mod; if (x < 0) x += mod; return x; }
 tuple<ll,ll,ll> exgcd(ll a, ll b) {
-  ll x = 1, y = 0;
-  ll x1 = 0, y1 = 1, a1 = a, b1 = b;
-  while (b1) {
-    ll q = a1 / b1;
-    tie(x, x1) = make_tuple(x1, x - q * x1);
-    tie(y, y1) = make_tuple(y1, y - q * y1);
-    tie(a1, b1) = make_tuple(b1, a1 - q * b1);
-  }
-  return {a1,x,y};
-}
-
-template<typename T>
-ll do_crt(vector<T>& a, vector<T>& m) {
-  ll ans = a[0];
-  ll l = m[0];
-  for(int i = 1; i < a.size(); i++) {
-    auto [d, x1, y1] = exgcd(l, m[i]);
-    if((a[i] - ans) % d != 0) {
-      return -1;
+    ll x = 1, y = 0;
+    ll x1 = 0, y1 = 1, a1 = a, b1 = b;
+    while (b1) {
+        ll q = a1 / b1;
+        tie(x, x1) = make_tuple(x1, x - q * x1);
+        tie(y, y1) = make_tuple(y1, y - q * y1);
+        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
     }
-    ans = normalize(ans + x1 * (a[i] - ans) / d % (m[i] / d) * l, l * m[i] / d);
-    l = lcm(l, m[i]);
-  }
-  return ans;
+    return {a1,x,y};
 }
 
 void solve() {
@@ -55,7 +39,19 @@ void solve() {
       }
     }
   }
-  cout << "! " << do_crt(a,m) << endl;
+  ll ans = a[0];
+  ll l = m[0];
+  for(int i = 1; i < a.size(); i++)
+  {
+    auto [d, x1, y1] = exgcd(l, m[i]);
+    if((a[i] - ans) % d != 0) {
+      cout << "error" << endl;
+    }
+    ans = normalize(ans + x1 * (a[i] - ans) / d % (m[i] / d) * l, l * m[i] / d);
+    l = lcm(l, m[i]);
+  }
+
+  cout << "! " << ans << endl;
 }
 
 int main() {
