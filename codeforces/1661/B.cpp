264a9c4b8439d02a45ca2142d858322849102ff7
Sunho Kim <ksunhokim123@gmail.com>
2022-04-10 00:13:00 +0900

    Add solution for problem `B - Getting Zero` Link: https://codeforces.com/contest/1661/problem/B Tags: bitmasks, brute force, dfs and similar, dp, graphs, greedy, shortest paths, *1300 Ref: https://codeforces.com/contest/1661/submission/153182839

--- codeforces/1661/B.cpp
+++ codeforces/1661/B.cpp
@@ -9,35 +9,33 @@ template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
 void solve() {
-  const int M = 32768;
   int n;
   cin >> n;
-  queue<pair<int,int>> q;
-  vector<int> dist(M+1, -1);
-  q.push({0, 0});
-  map<int,vector<int>> invs;
-  for(int i=1;i<=M;i++) {
-    invs[i*2%M].push_back(i);
-  }
-  while(!q.empty()){
-    auto [d, n] = q.front();
-    q.pop();
-    if (dist[n] == -1) {
-      dist[n] = d;
-    } else {
-      continue;
-    }
-    q.push({d+1, (n-1 + M) % M});
-    for(int k : invs[n]) {
-      q.push({d+1, k});
-    }
-  }
-  for(int i=0;i<n;i++) {
+  for(int i=0;i<n;i++){
     int a;
     cin >> a;
-    cout << dist[a] << " ";
+    int gans = 1e9;
+    if (a == 0) {
+      cout << 0 << " ";
+    } else {
+      const int M = 32768;
+      vector<pair<int,int>> cands;
+      for(int i=0;i<16;i++){
+        cands.push_back({a+i,i});
+      }
+      for(auto [cand,c] : cands) {
+        int ans = c;
+        cand %= M;
+        while (cand != 0) {
+          cand <<= 1;
+          cand %= M;
+          ans++;
+        }
+        gans = min(gans, ans);
+      }
+      cout << gans << " ";
+    }
   }
-  
 }
 
 int main() {
