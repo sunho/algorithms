416ffcebd2d84d1a0f82e7d9c36dd0cb07fbae69
Sunho Kim <ksunhokim123@gmail.com>
2022-04-11 17:02:00 +0900

    Add solution for problem `A - Equidistant Letters` Link: https://codeforces.com/contest/1626/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1626/submission/153359508

---
+++ codeforces/1626/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define x first
+#define y second
+#define all(x) begin(x), end(x)
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+void solve() {
+  string s;
+  cin >> s;
+  sort(all(s));
+  cout << s << "\n";
+}
+
+int main() {
+  cin.sync_with_stdio(0); cin.tie(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
