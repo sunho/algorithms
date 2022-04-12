520e8aa2fe00eac87b1d266f13e953296e74cc5b
Sunho Kim <ksunhokim123@gmail.com>
2022-04-12 10:21:00 +0900

    Add solution for problem `B - Update Files` Link: https://codeforces.com/contest/1606/problem/B Tags: greedy, implementation, math, *1100 Ref: https://codeforces.com/contest/1606/submission/153418201

---
+++ codeforces/1606/B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define x first
+#define y second
+#define all(x) begin(x), end(x)
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+void solve() {
+  ll n,k;
+  cin >> n >> k;
+  ll x = 1;
+  ll t = 0;
+  while (x < n) {
+    if (x < k) {
+      x *= 2;
+      t++;
+    } else {
+      t += (n-x+k-1)/k;
+      break;
+    }
+  }
+  cout << t << "\n";
+}
+
+int main() {
+  cin.sync_with_stdio(0); cin.tie(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
