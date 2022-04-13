686c981cd73c15ec71d2344c3a11c77868ad97f5
Sunho Kim <ksunhokim123@gmail.com>
2022-04-13 17:36:00 +0900

    Add solution for problem `E - Staircases` Link: https://codeforces.com/contest/1598/problem/E Tags: brute force, combinatorics, data structures, dfs and similar, dp, implementation, math, *2100 Ref: https://codeforces.com/contest/1598/submission/153535464

--- codeforces/1598/E.cpp
+++ codeforces/1598/E.cpp
@@ -12,72 +12,105 @@ void solve() {
   int n,m;
   cin >> n >> m;
   vector<vector<int>> M(n, vector<int>(m));
-  vector<vector<vector<ll>>> dp(n, vector<vector<ll>>(m, vector<ll>(3)));
+  auto is_valid = [&](pair<int,int> v) {
+    return v.x >= 0 && v.x < n && v.y >= 0 && v.y < m;
+  };
+  vector<vector<vector<ll>>> dp(n, vector<vector<ll>>(m, vector<ll>(2)));
   dp[0][0][0] = 0;
   dp[0][0][1] = 0;
-  for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-      dp[i][j][2] = 1;
-    }
-  }
   for(int i=0;i<n;i++) {
     for(int j=0;j<m;j++) {
       if (i==0 && j==0)continue;
-      if (i != 0) dp[i][j][1] = dp[i-1][j][0]+dp[i-1][j][2];
-      if (j != 0) dp[i][j][0] = dp[i][j-1][1]+dp[i][j-1][2];
+      if (i != 0) {
+        dp[i][j][1] = dp[i-1][j][0]+1;
+      }
+      if (j != 0) {
+        dp[i][j][0] = dp[i][j-1][1]+1;
+      }
     }
   }
-  ll cur = 0;
+  ll cur = n*m;
   for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
-      for(int k=0;k<3;k++) {
+      for(int k=0;k<2;k++) {
         cur += dp[i][j][k];
       }
     }
   }
+
   auto set_dp = [&](int i,int j, int k, ll val) {
     cur -= dp[i][j][k];
     dp[i][j][k] = val;
     cur += val;
   };
+
   auto update_dp = [&](int i, int j, int k) {
-    if (M[i][j] == 1) {
-      set_dp(i,j,k,0);
-      return;
+    if (k == 1) {
+      if (i != 0) {
+        if (M[i-1][j] == 1 || M[i][j] == 1) {
+          set_dp(i,j,k, 0);
+        } else {
+          set_dp(i,j,k, dp[i-1][j][k^1]+1);
+        }
+      }
+    } else {
+      if (j != 0) {
+        if (M[i][j-1] == 1 || M[i][j] == 1) {
+          set_dp(i,j,k,0);
+        } else {
+          set_dp(i,j,k, dp[i][j-1][k^1]+1);
+        }
+      }
     }
-    if (k == 1) if (i != 0) set_dp(i,j,1, dp[i-1][j][0]+dp[i-1][j][2]);
-    if (k == 0) if (j != 0) set_dp(i,j,0, dp[i][j-1][1]+dp[i][j-1][2]);
   };
+
+  auto update_stair = [&](int x, int y, int step, int del) {
+    ll sum = 0;
+    pair<int,int> s = {x,y};
+    auto t = s;
+    while (t.x >= 0 && t.x < n && t.y >= 0 && t.y < m) {
+      // if (t!=s && M[t.x][t.y] == 1) {
+      //   break;
+      // }
+      if (t != s) {
+        update_dp(t.x,t.y,0);
+        update_dp(t.x,t.y,1);
+      }
+      if (step == 0) {
+        t.y += del;
+      } else {
+        t.x += del;
+      }
+      step ^= 1;
+    }
+  };
+
+  
+  auto update = [&](int x, int y, int c) {
+    M[x][y] = c;
+    ll old = 0;
+    if (c == 1) {
+      set_dp(x,y,0, 0);
+      set_dp(x,y,1, 0);
+      cur--;
+    } else {
+      update_dp(x,y,0);
+      update_dp(x,y,1);
+      cur++;
+    }
+    update_stair(x,y,0,1);
+    update_stair(x,y,1,1);
+
+  };
+
+
   int q;
   cin >> q;
   while(q--){
     int x,y;
     cin >> x >> y;
     --x,--y;
-    M[x][y]^=1;
-    ll old = 0;
-    if (M[x][y] == 1) {
-      set_dp(x,y,0,0);
-      set_dp(x,y,1,0);
-      set_dp(x,y,2,0);
-    } else {
-      set_dp(x,y,2,1);
-    }
-    for (int k = 0; k < 2; k++) {
-      int step = k;
-      int i = x;
-      int j = y;
-      while (i >= 0 && i < n && j >= 0 && j < m) {
-        update_dp(i,j,0);
-        update_dp(i,j,1);
-        if (step == 0) {
-          i += 1;
-        } else {
-          j += 1;
-        }
-        step ^= 1;
-      }
-    }
+    update(x,y,M[x][y]^1);
     cout << cur << "\n";
   }
 }
