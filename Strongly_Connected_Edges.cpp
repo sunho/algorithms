3e8a71e9f3f5eeec601760ed70c1886e9c777fc1
Sunho Kim <ksunhokim123@gmail.com>
2023-03-29 01:28:09 -0700

    Update

---
+++ Strongly_Connected_Edges.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  int n,m;
+  cin >> n >> m;
+  vector adj(n, vector<int>());
+  
+}
