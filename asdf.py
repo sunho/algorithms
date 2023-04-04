fe7a2cbbb0a8fc70c97fb0c2027fda4c2a9d27da
Sunho Kim <ksunhokim123@gmail.com>
2023-04-04 03:05:57 -0700

    Cleanup

--- asdf.py
+++
@@ -1,8 +0,0 @@
-from random import randrange
-
-
-from subprocess import check_output
-
-with open("/home/user/file.in", "rb") as file:
-    output = check_output(["/home/user/executable"], stdin=file)
-    
